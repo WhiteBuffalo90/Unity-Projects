c67b2f5dfc9935fa8cb1a82b2c9cc1ae4027fbd2
WhiteBuffalo90 <37558551+WhiteBuffalo90@users.noreply.github.com>
2019-01-24 20:33:51 -0700

    Figured out Idle income

--- Experimenting Project/Assets/Scripts/Game_Controller.cs
+++ Experimenting Project/Assets/Scripts/Game_Controller.cs
@@ -3,20 +3,89 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class Game_Controller : MonoBehaviour {
 
 
-    private double basicMoney = 0f;
+    public double basicMoney = 0f;
 
-    TextMeshProUGUI basicMoneyText;
+    public TextMeshProUGUI basicMoneyText;
+
+    private string abvValue;
 
     private void Update()
     {
 
-        basicMoney += 5.123f;
+        basicMoney += 5000000000000.123f;
+        
+        
 
-        Debug.Log(basicMoney.ToString(""));
+        if(basicMoney > 1000 && basicMoney < 1000000)
+        {
+            abvValue = "K";
+            
+            basicMoneyText.text =  Math.Floor(basicMoney / 1000).ToString("F0") + abvValue;
+             
+        }
+        else if(basicMoney >= 1000000 && basicMoney < 1000000000)
+        {
+            abvValue = "M";
+
+            basicMoneyText.text = Math.Floor(basicMoney / 1000000).ToString("F0") + abvValue;
+        }
+        else if (basicMoney >= 1000000000 && basicMoney < 1000000000000)
+        {
+            abvValue = "B";
+
+            basicMoneyText.text = Math.Floor(basicMoney / 1000000000).ToString("F0") + abvValue;
+        }
+        else if (basicMoney >= 1000000000000 && basicMoney < 1000000000000000)
+        {
+            abvValue = "T";
+
+            basicMoneyText.text = Math.Floor(basicMoney / 1000000000000).ToString("0") + abvValue;
+        }
+        else if (basicMoney >= 1000000000000000 && basicMoney < 1000000000000000000)
+        {
+            abvValue = "a";
+
+            basicMoneyText.text = Math.Floor(basicMoney / 1000000000000000).ToString("0") + abvValue;
+        }
+        else if (basicMoney >= 1000000000000000000 && basicMoney < 1000000000000000000000f)
+        {
+            abvValue = "b";
+
+            basicMoneyText.text = Math.Floor(basicMoney / 1000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (basicMoney >= 1000000000000000000000f && basicMoney < 1000000000000000000000000f)
+        {
+            abvValue = "c";
+
+            basicMoneyText.text = Math.Floor(basicMoney / 1000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (basicMoney >= 1000000000000000000000000f && basicMoney < 1000000000000000000000000000f)
+        {
+            abvValue = "d";
+
+            basicMoneyText.text = Math.Floor(basicMoney / 1000000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (basicMoney >= 1000000000000000000000000000f && basicMoney < 1000000000000000000000000000000f)
+        {
+            abvValue = "e";
+
+            basicMoneyText.text = Math.Floor(basicMoney / 1000000000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (basicMoney >= 1000000000000000000000000000000f && basicMoney < 1000000000000000000000000000000000f)
+        {
+            abvValue = "f";
+
+            basicMoneyText.text = Math.Floor(basicMoney / 1000000000000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else
+        {
+            basicMoneyText.text = basicMoney.ToString("F0");
+        }
 
     }
 
