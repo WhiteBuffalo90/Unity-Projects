43a2fa5045fd4251dd819b9cc8c5326b50066855
WhiteBuffalo90 <37558551+WhiteBuffalo90@users.noreply.github.com>
2019-02-01 23:36:16 -0700

    idle updates
    All idle currencies up and some UI addtions

--- Experimenting Project/Assets/Scripts/Game_Controller.cs
+++ Experimenting Project/Assets/Scripts/Game_Controller.cs
@@ -1,90 +1,388 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using System;
 using TMPro;
-using System;
+using UnityEngine;
 
-public class Game_Controller : MonoBehaviour {
+public class Game_Controller : MonoBehaviour
+{
 
 
-    public double basicMoney = 0f;
+    public double woodCur = 0f;
+    public double stoneCur = 0f;
+    public double metalCur = 0f;
+    public double fuelCur = 0f;
+    public double powerCur = 0f;
 
-    public TextMeshProUGUI basicMoneyText;
+    public TextMeshProUGUI woodText;
+    public TextMeshProUGUI stoneText;
+    public TextMeshProUGUI metalText;
+    public TextMeshProUGUI fuelText;
+    public TextMeshProUGUI powerText;
+
+    public float woodTime = 3.0f;
+    public float stoneTime = 3.5f;
+    public float metalTime = 4.5f;
+    public float fuelTime = 5.5f;
+    public float powerTime = 6.5f;
 
     private string abvValue;
 
-    private void Update()
+    private void Awake()
     {
+        InvokeRepeating("UpdateWood", 0, woodTime);
+        InvokeRepeating("UpdateStone", 0, stoneTime);
+        InvokeRepeating("UpdateMetal", 0, metalTime);
+        InvokeRepeating("UpdateFuel", 0, fuelTime);
+        InvokeRepeating("UpdatePower", 0, powerTime);
+    }
 
-        basicMoney += 5000000000000.123f;
-        
-        
 
-        if(basicMoney > 1000 && basicMoney < 1000000)
+    public void UpdateWood()
+    {
+        woodCur += 5.123f;
+
+        if (woodCur > 1000 && woodCur < 1000000)
         {
             abvValue = "K";
-            
-            basicMoneyText.text =  Math.Floor(basicMoney / 1000).ToString("F0") + abvValue;
-             
+
+            woodText.text = Math.Floor(woodCur / 1000).ToString("F0") + abvValue;
         }
-        else if(basicMoney >= 1000000 && basicMoney < 1000000000)
+        else if (woodCur >= 1000000 && woodCur < 1000000000)
         {
             abvValue = "M";
 
-            basicMoneyText.text = Math.Floor(basicMoney / 1000000).ToString("F0") + abvValue;
+            woodText.text = Math.Floor(woodCur / 1000000).ToString("F0") + abvValue;
         }
-        else if (basicMoney >= 1000000000 && basicMoney < 1000000000000)
+        else if (woodCur >= 1000000000 && woodCur < 1000000000000)
         {
             abvValue = "B";
 
-            basicMoneyText.text = Math.Floor(basicMoney / 1000000000).ToString("F0") + abvValue;
+            woodText.text = Math.Floor(woodCur / 1000000000).ToString("F0") + abvValue;
         }
-        else if (basicMoney >= 1000000000000 && basicMoney < 1000000000000000)
+        else if (woodCur >= 1000000000000 && woodCur < 1000000000000000)
         {
             abvValue = "T";
 
-            basicMoneyText.text = Math.Floor(basicMoney / 1000000000000).ToString("0") + abvValue;
+            woodText.text = Math.Floor(woodCur / 1000000000000).ToString("0") + abvValue;
         }
-        else if (basicMoney >= 1000000000000000 && basicMoney < 1000000000000000000)
+        else if (woodCur >= 1000000000000000 && woodCur < 1000000000000000000)
         {
             abvValue = "a";
 
-            basicMoneyText.text = Math.Floor(basicMoney / 1000000000000000).ToString("0") + abvValue;
+            woodText.text = Math.Floor(woodCur / 1000000000000000).ToString("0") + abvValue;
         }
-        else if (basicMoney >= 1000000000000000000 && basicMoney < 1000000000000000000000f)
+        else if (woodCur >= 1000000000000000000 && woodCur < 1000000000000000000000f)
         {
             abvValue = "b";
 
-            basicMoneyText.text = Math.Floor(basicMoney / 1000000000000000000f).ToString("F0") + abvValue;
+            woodText.text = Math.Floor(woodCur / 1000000000000000000f).ToString("F0") + abvValue;
         }
-        else if (basicMoney >= 1000000000000000000000f && basicMoney < 1000000000000000000000000f)
+        else if (woodCur >= 1000000000000000000000f && woodCur < 1000000000000000000000000f)
         {
             abvValue = "c";
 
-            basicMoneyText.text = Math.Floor(basicMoney / 1000000000000000000000f).ToString("F0") + abvValue;
+            woodText.text = Math.Floor(woodCur / 1000000000000000000000f).ToString("F0") + abvValue;
         }
-        else if (basicMoney >= 1000000000000000000000000f && basicMoney < 1000000000000000000000000000f)
+        else if (woodCur >= 1000000000000000000000000f && woodCur < 1000000000000000000000000000f)
         {
             abvValue = "d";
 
-            basicMoneyText.text = Math.Floor(basicMoney / 1000000000000000000000000f).ToString("F0") + abvValue;
+            woodText.text = Math.Floor(woodCur / 1000000000000000000000000f).ToString("F0") + abvValue;
         }
-        else if (basicMoney >= 1000000000000000000000000000f && basicMoney < 1000000000000000000000000000000f)
+        else if (woodCur >= 1000000000000000000000000000f && woodCur < 1000000000000000000000000000000f)
         {
             abvValue = "e";
 
-            basicMoneyText.text = Math.Floor(basicMoney / 1000000000000000000000000000f).ToString("F0") + abvValue;
+            woodText.text = Math.Floor(woodCur / 1000000000000000000000000000f).ToString("F0") + abvValue;
         }
-        else if (basicMoney >= 1000000000000000000000000000000f && basicMoney < 1000000000000000000000000000000000f)
+        else if (woodCur >= 1000000000000000000000000000000f && woodCur < 1000000000000000000000000000000000f)
         {
             abvValue = "f";
 
-            basicMoneyText.text = Math.Floor(basicMoney / 1000000000000000000000000000000f).ToString("F0") + abvValue;
+            woodText.text = Math.Floor(woodCur / 1000000000000000000000000000000f).ToString("F0") + abvValue;
         }
         else
         {
-            basicMoneyText.text = basicMoney.ToString("F0");
+            woodText.text = woodCur.ToString("F0");
+        }
+
+    }
+    public void UpdateStone()
+    {
+        stoneCur += 5.123f;
+
+        if (stoneCur > 1000 && stoneCur < 1000000)
+        {
+            abvValue = "K";
+
+            stoneText.text = Math.Floor(stoneCur / 1000).ToString("F0") + abvValue;
+        }
+        else if (stoneCur >= 1000000 && stoneCur < 1000000000)
+        {
+            abvValue = "M";
+
+            stoneText.text = Math.Floor(stoneCur / 1000000).ToString("F0") + abvValue;
+        }
+        else if (stoneCur >= 1000000000 && stoneCur < 1000000000000)
+        {
+            abvValue = "B";
+
+            stoneText.text = Math.Floor(stoneCur / 1000000000).ToString("F0") + abvValue;
+        }
+        else if (stoneCur >= 1000000000000 && stoneCur < 1000000000000000)
+        {
+            abvValue = "T";
+
+            stoneText.text = Math.Floor(stoneCur / 1000000000000).ToString("0") + abvValue;
+        }
+        else if (stoneCur >= 1000000000000000 && stoneCur < 1000000000000000000)
+        {
+            abvValue = "a";
+
+            stoneText.text = Math.Floor(stoneCur / 1000000000000000).ToString("0") + abvValue;
+        }
+        else if (stoneCur >= 1000000000000000000 && stoneCur < 1000000000000000000000f)
+        {
+            abvValue = "b";
+
+            stoneText.text = Math.Floor(stoneCur / 1000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (stoneCur >= 1000000000000000000000f && stoneCur < 1000000000000000000000000f)
+        {
+            abvValue = "c";
+
+            stoneText.text = Math.Floor(stoneCur / 1000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (stoneCur >= 1000000000000000000000000f && stoneCur < 1000000000000000000000000000f)
+        {
+            abvValue = "d";
+
+            stoneText.text = Math.Floor(stoneCur / 1000000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (stoneCur >= 1000000000000000000000000000f && stoneCur < 1000000000000000000000000000000f)
+        {
+            abvValue = "e";
+
+            stoneText.text = Math.Floor(stoneCur / 1000000000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (stoneCur >= 1000000000000000000000000000000f && stoneCur < 1000000000000000000000000000000000f)
+        {
+            abvValue = "f";
+
+            stoneText.text = Math.Floor(stoneCur / 1000000000000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else
+        {
+            stoneText.text = stoneCur.ToString("F0");
+        }
+
+    }
+    public void UpdateMetal()
+    {
+        metalCur += 2.123f;
+
+        if (metalCur > 1000 && metalCur < 1000000)
+        {
+            abvValue = "K";
+
+            metalText.text = Math.Floor(metalCur / 1000).ToString("F0") + abvValue;
+        }
+        else if (metalCur >= 1000000 && metalCur < 1000000000)
+        {
+            abvValue = "M";
+
+            metalText.text = Math.Floor(metalCur / 1000000).ToString("F0") + abvValue;
+        }
+        else if (metalCur >= 1000000000 && metalCur < 1000000000000)
+        {
+            abvValue = "B";
+
+            metalText.text = Math.Floor(metalCur / 1000000000).ToString("F0") + abvValue;
+        }
+        else if (metalCur >= 1000000000000 && metalCur < 1000000000000000)
+        {
+            abvValue = "T";
+
+            metalText.text = Math.Floor(metalCur / 1000000000000).ToString("0") + abvValue;
+        }
+        else if (metalCur >= 1000000000000000 && metalCur < 1000000000000000000)
+        {
+            abvValue = "a";
+
+            metalText.text = Math.Floor(metalCur / 1000000000000000).ToString("0") + abvValue;
+        }
+        else if (metalCur >= 1000000000000000000 && metalCur < 1000000000000000000000f)
+        {
+            abvValue = "b";
+
+            metalText.text = Math.Floor(metalCur / 1000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (metalCur >= 1000000000000000000000f && metalCur < 1000000000000000000000000f)
+        {
+            abvValue = "c";
+
+            metalText.text = Math.Floor(metalCur / 1000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (metalCur >= 1000000000000000000000000f && metalCur < 1000000000000000000000000000f)
+        {
+            abvValue = "d";
+
+            metalText.text = Math.Floor(metalCur / 1000000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (metalCur >= 1000000000000000000000000000f && metalCur < 1000000000000000000000000000000f)
+        {
+            abvValue = "e";
+
+            metalText.text = Math.Floor(metalCur / 1000000000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (metalCur >= 1000000000000000000000000000000f && metalCur < 1000000000000000000000000000000000f)
+        {
+            abvValue = "f";
+
+            stoneText.text = Math.Floor(metalCur / 1000000000000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else
+        {
+            metalText.text = metalCur.ToString("F0");
+        }
+
+    }
+    public void UpdateFuel()
+    {
+        fuelCur += 1.423f;
+
+        if (fuelCur > 1000 && fuelCur < 1000000)
+        {
+            abvValue = "K";
+
+            fuelText.text = Math.Floor(fuelCur / 1000).ToString("F0") + abvValue;
+        }
+        else if (fuelCur >= 1000000 && fuelCur < 1000000000)
+        {
+            abvValue = "M";
+
+            fuelText.text = Math.Floor(fuelCur / 1000000).ToString("F0") + abvValue;
+        }
+        else if (fuelCur >= 1000000000 && fuelCur < 1000000000000)
+        {
+            abvValue = "B";
+
+            fuelText.text = Math.Floor(fuelCur / 1000000000).ToString("F0") + abvValue;
+        }
+        else if (fuelCur >= 1000000000000 && fuelCur < 1000000000000000)
+        {
+            abvValue = "T";
+
+            fuelText.text = Math.Floor(fuelCur / 1000000000000).ToString("0") + abvValue;
+        }
+        else if (fuelCur >= 1000000000000000 && fuelCur < 1000000000000000000)
+        {
+            abvValue = "a";
+
+            fuelText.text = Math.Floor(fuelCur / 1000000000000000).ToString("0") + abvValue;
+        }
+        else if (fuelCur >= 1000000000000000000 && fuelCur < 1000000000000000000000f)
+        {
+            abvValue = "b";
+
+            fuelText.text = Math.Floor(fuelCur / 1000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (fuelCur >= 1000000000000000000000f && fuelCur < 1000000000000000000000000f)
+        {
+            abvValue = "c";
+
+            fuelText.text = Math.Floor(fuelCur / 1000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (fuelCur >= 1000000000000000000000000f && fuelCur < 1000000000000000000000000000f)
+        {
+            abvValue = "d";
+
+            fuelText.text = Math.Floor(fuelCur / 1000000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (fuelCur >= 1000000000000000000000000000f && fuelCur < 1000000000000000000000000000000f)
+        {
+            abvValue = "e";
+
+            fuelText.text = Math.Floor(fuelCur / 1000000000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (fuelCur >= 1000000000000000000000000000000f && fuelCur < 1000000000000000000000000000000000f)
+        {
+            abvValue = "f";
+
+            fuelText.text = Math.Floor(fuelCur / 1000000000000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else
+        {
+            fuelText.text = fuelCur.ToString("F0");
+        }
+
+    }
+    public void UpdatePower()
+    {
+        powerCur += 1.123f;
+
+        if (powerCur > 1000 && powerCur < 1000000)
+        {
+            abvValue = "K";
+
+            powerText.text = Math.Floor(powerCur / 1000).ToString("F0") + abvValue;
+        }
+        else if (powerCur >= 1000000 && powerCur < 1000000000)
+        {
+            abvValue = "M";
+
+            powerText.text = Math.Floor(powerCur / 1000000).ToString("F0") + abvValue;
+        }
+        else if (powerCur >= 1000000000 && powerCur < 1000000000000)
+        {
+            abvValue = "B";
+
+            powerText.text = Math.Floor(powerCur / 1000000000).ToString("F0") + abvValue;
+        }
+        else if (powerCur >= 1000000000000 && powerCur < 1000000000000000)
+        {
+            abvValue = "T";
+
+            powerText.text = Math.Floor(powerCur / 1000000000000).ToString("0") + abvValue;
+        }
+        else if (powerCur >= 1000000000000000 && powerCur < 1000000000000000000)
+        {
+            abvValue = "a";
+
+            powerText.text = Math.Floor(powerCur / 1000000000000000).ToString("0") + abvValue;
+        }
+        else if (powerCur >= 1000000000000000000 && powerCur < 1000000000000000000000f)
+        {
+            abvValue = "b";
+
+            powerText.text = Math.Floor(powerCur / 1000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (powerCur >= 1000000000000000000000f && powerCur < 1000000000000000000000000f)
+        {
+            abvValue = "c";
+
+            powerText.text = Math.Floor(powerCur / 1000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (powerCur >= 1000000000000000000000000f && powerCur < 1000000000000000000000000000f)
+        {
+            abvValue = "d";
+
+            powerText.text = Math.Floor(powerCur / 1000000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (powerCur >= 1000000000000000000000000000f && powerCur < 1000000000000000000000000000000f)
+        {
+            abvValue = "e";
+
+            powerText.text = Math.Floor(powerCur / 1000000000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else if (powerCur >= 1000000000000000000000000000000f && powerCur < 1000000000000000000000000000000000f)
+        {
+            abvValue = "f";
+
+            powerText.text = Math.Floor(powerCur / 1000000000000000000000000000000f).ToString("F0") + abvValue;
+        }
+        else
+        {
+            powerText.text = powerCur.ToString("F0");
         }
 
     }
